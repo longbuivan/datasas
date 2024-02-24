a9205d2366ebc7bb69d4900fec442d5206d355c2
longbuivan <longbuivan95@gmail.com>
2024-02-25 00:49:25 +0700

    Structure Project

--- infra/kafka/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t kafka-broker .
-
-docker run -p 9092:9092 --name kafka-broker kafka-broker
--- infra/prometheus/install.sh
+++
@@ -1,3 +0,0 @@
-echo "Running to install KrakenD"
-docker run -p 9090:9090 prom/prometheus
-echo "Finished launch KrakenD"
